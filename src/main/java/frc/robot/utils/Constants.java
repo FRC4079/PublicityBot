298a1140cfb04a175b8c82cac2220c5f4b6f689a
ntnx0246 <shawngazin@protonmail.com>
2023-01-11 12:23:00 -0800

    changed canids to match pub bot

--- src/main/java/frc/robot/utils/Constants.java
+++ src/main/java/frc/robot/utils/Constants.java
@@ -3,10 +3,10 @@ package frc.robot.utils;
 public final class Constants {
 
     public final static class ID {
-        public static final int DRIVETRAIN_FRONT_LEFT = 0;
-        public static final int DRIVETRAIN_FRONT_RIGHT = 1;
-        public static final int DRIVETRAIN_BACK_LEFT = 2;
-        public static final int DRIVETRAIN_BACK_RIGHT = 3;
+        public static final int DRIVETRAIN_FRONT_LEFT = 10;
+        public static final int DRIVETRAIN_FRONT_RIGHT = 7;
+        public static final int DRIVETRAIN_BACK_LEFT = 6;
+        public static final int DRIVETRAIN_BACK_RIGHT = 8;
     }
 
     public final static class DriveTrain {
